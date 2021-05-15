378dbe3116af1afcea219c0afc8c69a5cc0f0c3f
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-05-15 17:05:32 +0100

    Migrate to single file setup

--- main.js
+++
@@ -1,9 +0,0 @@
-const addItem = (id, content) => {
-  const itemContainer = document.createElement("div");
-  itemContainer.innerHTML = content;
-  document.body.appendChild(itemContainer);
-};
-
-for (let i = 0; i < 45; i++) {
-  addItem(i, "<p>Test item " + i + "</p>");
-}
